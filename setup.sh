ae91999dd9f2ea02093f05907bfdb2a0050206a1
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-06-16 19:56:52 +0400

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-CRON_JOB="25 * * * * ./run.sh"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ABSOLUTE_RUN_SH="$SCRIPT_DIR/run.sh"
+
+CRON_JOB="25 * * * * $ABSOLUTE_RUN_SH"
+
 CRON_EXISTS=$(crontab -l 2>/dev/null | grep -F "$CRON_JOB")
 
 sudo apt update
-sudo apt install cron
+sudo apt install -y cron
 sudo systemctl enable cron
 sudo systemctl start cron
-mkdir research
+
+mkdir -p research
 python3 -m venv venv
-pip install -r requirments.txt
+source venv/bin/activate
+pip install -r requirements.txt
+
 if [ -z "$CRON_EXISTS" ]; then
     (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
     echo "Cron job added."
