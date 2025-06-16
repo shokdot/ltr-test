42c27ca949ae4086ab0472e20e2429125a69198f
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-06-16 17:04:10 +0400

    fix:bash file

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 source ./venv/bin/activate
 python3 ltr390.py
--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+
+CRON_JOB="25 * * * * ./run.sh"
+CRON_EXISTS=$(crontab -l 2>/dev/null | grep -F "$CRON_JOB")
+
 sudo apt update
 sudo apt install cron
 sudo systemctl enable cron
 sudo systemctl start cron
 mkdir research
 python3 -m venv venv
-@if [ -z "$(CRON_EXISTS)" ]; then \
-	(crontab -l 2>/dev/null; echo $(CRON_JOB)) | crontab -; \
-	echo "Cron job added."; \
-else \
-	echo "Cron job already exists."; \
+if [ -z "$CRON_EXISTS" ]; then
+    (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+    echo "Cron job added."
+else
+    echo "Cron job already exists."
 fi
